b33da0d0351f834382a5aa7a83cee8a3c22eb16c
AndyOlsen1295 <112365233+AndyOlsen1295@users.noreply.github.com>
2023-04-17 12:16:09 -0600

    added mongoDB and yarn

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('../schemas/typeDefs');
 const books = require('./models/book');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
 
 const resolvers = {
   Query: {
@@ -18,6 +22,15 @@ async function startServer() {
   const app = express();
   await server.start();
   server.applyMiddleware({ app });
+
+  const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  // Connect to the MongoDB database and start the server
+  await client.connect();
+  const db = client.db('mydb');
+  const collection = db.collection('users');
+  console.log('Connected to MongoDB');
+
   const PORT = 4000;
   app.listen({ port: PORT }, () => {
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
